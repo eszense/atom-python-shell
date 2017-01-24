55e02d6ad7e6fadb6837dea18e9d162e0bf357ea
eszense <eszense@users.noreply.github.com>
2017-01-25 07:49:25 +0800

    New Atom Package

---
+++ spec/atom-python-shell-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomPythonShellView from '../lib/atom-python-shell-view';
+
+describe('AtomPythonShellView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
